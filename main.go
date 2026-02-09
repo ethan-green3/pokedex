a4ce9f3d2bbc09d39eec2c9cbcf9d65cf4945425
ethan-green3 <ethangreen2000@yahoo.com>
2026-02-09 10:04:52 -0600

    Basic REPL implemented

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		cleanedInput := cleanInput(userInput)
+		firstWord := cleanedInput[0]
+		fmt.Println("Your command was:", firstWord)
+	}
 }
